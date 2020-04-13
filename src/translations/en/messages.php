d8779d27cee267e477ae79e1d112eea6280bda8c
Kennedy Kioko <kmkioko@gmail.com>
2020-04-13 11:36:24 +0300

    String Translations
    OTP string translations in English

--- src/translations/en/messages.php
+++ src/translations/en/messages.php
@@ -11,18 +11,12 @@ return [
     | messages that we need to display to the user.
     |
     */
-
-    "verify_phone_title" => "Verify Your Phone Number",
-    "verify_phone_button" => "Verify",
-    "one_time_password" => "One Time Password (OTP)",
-    "hero_text" => "Thanks for giving your details. An OTP has been sent to your Mobile Number. Please enter the :digit digit OTP below for Successful Login",
-    "cancel_otp" => "Nevermind, take me back to the login page.",
-    "otp_expired" => "Your OTP code seems to be expired. Please login again to get a new OTP code.",
-    "unauthorized" => "You are not allowed to do this.",
-    "code_missing" => "The OTP field should exist and be filled.",
-    "code_mismatch" => "The code doesn't match to the generated one. Please check and re-enter the code you received.",
-    "otp_message" => "Your one-time password to enter the system is: :password",
-    "otp_not_received" => "Didn't receive the password? Log out and try again!",
-    "service_not_responding" => "SMS service currently seems to be not responding. Please try again."
+    "otp_message" => "Your one-time password (OTP) to enter the system is: :password",
+    "otp_valid" => "The one-time password (OTP) token is valid.",
+    "otp_invalid" => "The one-time password (OTP) token is not valid.",
+    "otp_missing" => "The one-time password (OTP) token does not exist.",
+    "otp_expired" => "Your one-time password (OTP) token seems to be expired. Please request a new OTP code.",
+    "otp_extended" => "Your one-time password (OTP) token expiry was extended by :minutes mins",
+    "code_mismatch" => "The one-time password (OTP) token doesn't match any token in database.",
 
 ];
